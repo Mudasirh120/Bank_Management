46b39e5cbd7fe6d23ac49a347439b427ca6df5f9
M10SIR <mudasirh120@gmail.com>
2024-05-28 06:48:32 +0000

    Basic Menu

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+    public static void displayMenu(){
+        System.out.println("Welcome ");
+        System.out.println("Choose what to do ");
+        System.out.println("1. View Details");
+        System.out.println("2. Reset Pin");
+        System.out.println("3. Upgrade Card");
+        System.out.println("4. Change Account Type");
+        System.out.println("5. Deposit Cash");
+        System.out.println("6. Withdraw Cash ");
+        System.out.println("7. Check Balance ");
+        System.out.println("8. Logout");
+    }
+}
