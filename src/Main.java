433306fc65ce671a864a1f3e6d2d1000ab7d5ba2
Daniyal Sarwar <55481@student.riphah.edu.pk>
2024-05-28 17:11:35 +0500

    Saving Account Functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
        Login.loginPage();
+
+        SavingAccount savingAccount = new SavingAccount("SA123", "Alice", 1000.0, 5.0);
+        CurrentAccount currentAccount = new CurrentAccount("CA123", "Bob", 500.0, 1000.0);
     }
 }
+
--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -1,2 +1,18 @@
-public class SavingAccount {
+public class SavingAccount extends Account {
+    private double interestRate;
+
+    public SavingAccount(String accountNumber, String accountHolderName, double initialBalance, double interestRate) {
+        super(accountNumber, accountHolderName, initialBalance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void calculateInterest() {
+        double interest = getBalance() * interestRate / 100;
+        deposit(interest);
+        System.out.println("Interest added: " + interest);
+    }
+
+    // Additional methods specific to SavingAccount can be added here
 }
+
