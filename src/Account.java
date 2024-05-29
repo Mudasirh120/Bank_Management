80eb77e7e3cd13efe942eeee68e09434c3b90d95
M10SIR <mudasirh120@gmail.com>
2024-05-29 10:25:43 +0500

    Added Functionality of changing information

--- src/Account.java
+++ src/Account.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Account{
     public static void displayAccount(){
         Scanner scanner= new Scanner(System.in);
-        String password;
+        String password="";
         while(!password.equals(Login.getStoredPassword())) {
             System.out.println("Enter your password to display information.");
             password = scanner.nextLine();
@@ -17,12 +17,50 @@ class Account{
             }
         }
             if (password.equals(Login.getStoredPassword())) {
+                char c;
                 System.out.println("Displaying Information.");
                 System.out.println("Name : " + Login.getStoredUsername());
                 System.out.println("Account Number : " + Login.getAccountNumber());
                 System.out.println("Password : " + Login.getStoredPassword());
                 System.out.println("Recovery Key : " + Login.getRecoverykey());
                 System.out.println("Transaction Pin : " + Login.getPin());
+                System.out.println("press C to change details and Y to go back");
+                c=scanner.nextLine().charAt(0);
+                if(c == 'c' || c =='C'){
+                    changeInfo();
+                }
+                else{
+                    Menu.displayMenu();
+                }
             }
+    }
+    public static void changeInfo(){
+        Scanner scanner= new Scanner(System.in);
+        String storesUsername;
+        String storesPassword;
+        String recoverkey;
+        int c;
+        System.out.println("1.Name : " + Login.getStoredUsername());
+        System.out.println("2.Password : " + Login.getStoredPassword());
+        System.out.println("3.Recovery Key : " + Login.getRecoverykey());
+        System.out.println("Enter a Number corresponding to info to change it.");
+        c=scanner.nextInt();
+        switch (c){
+            case 1:
+                System.out.println("Enter a New Name.");
+                storesUsername=scanner.nextLine();
+                Login.setStoredUsername(storesUsername);
+                break;
+            case 2:
+                System.out.println("Enter a New Password.");
+                storesPassword=scanner.nextLine();
+                Login.setStoredPassword(storesPassword);
+                break;
+            case 3:
+                System.out.println("Enter a New Recovery Key.");
+                recoverkey=scanner.nextLine();
+                Login.setRecoverykey(recoverkey);
+                break;
+        }
     }
 }
